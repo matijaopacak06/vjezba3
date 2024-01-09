369c2963e1add6ac11f834c8dd86bf3c59f7c9c7
matijaopacak06 <153612293+matijaopacak06@users.noreply.github.com>
2024-01-09 12:25:37 +0100

    Add files via upload

---
+++ vjezba.03.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vjezba03
+{
+    class stablo
+    {
+        bool otpadajuListovi;
+        string vrstaStabla;
+
+        public bool OtpadajuListovi { get => otpadajuListovi; set => otpadajuListovi = value; }
+        public string VrstaStabla { get => vrstaStabla; set => vrstaStabla = value; }
+    }
+    public stablo (bool otpadajuListovi)
+    {
+        this.otpadajuListovi = otpadajuListovi;
+    }
+    public override string ToString()
+    {
+        string ispis = "ovo je stablo";
+        if (otpadajuListovi)
+        {
+            ispis += "Listopadno";
+        }
+        else
+        {
+            ispis += "zimzeleno";
+        }
+        return ispis;
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stablo stablo = new stablo(false);
+            stablo stablo2 = new stablo(true);
+            Console.WriteLine(stablo.ToString());
+            Console.WriteLine(stablo.ToString());
+            Console.ReadLine();
+        }
+    }
+}
